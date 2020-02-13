6cdd762a59069792332470faf2ebe380db038cd7
Ashley Boone <ashley@Ashleys-MacBook-Pro.local>
2020-02-12 19:00:52 -0800

    portfolio site

---
+++ docs/precache-manifest.7dfde43f2ef7eed66adf628dd77b936a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b846d8a4e98550b1ccdeb9c3575be39",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "382129dfbfd42324cc63",
+    "url": "/portfolio/static/css/main.4979d6ad.chunk.css"
+  },
+  {
+    "revision": "3f7c1abb021fc6586ab9",
+    "url": "/portfolio/static/js/2.9bc43008.chunk.js"
+  },
+  {
+    "revision": "382129dfbfd42324cc63",
+    "url": "/portfolio/static/js/main.db8cd3f5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+  name: "Ashley Boone",
+  role: "User Experience Research and Design",
+  linkedinId:"evanfeenstra",
+  skypeid: "Your skypeid",
+  roleDescription: "Hello! I am a UX Designer with a strong background in user research. I'm passionate about using a thoughtful, evidence-based approach to design to contribute to meaningful projects with a positive impact.",
+  socialLinks:[
+      {
+        name:"linkedin",
+        url:"https://www.linkedin.com/in/ashleynboone/",
+        className:"fa fa-linkedin"
+      },
+      {
+        name:"github",
+        url:"https://github.com/boone1000",
+        className:"fa fa-github"
+      },
+      // {
+      //   name:"skype",
+      //   url:"http://skype.com",
+      //   className:"fa fa-skype"
+      // }
+    ],
+  aboutme:"I have finished my B.S. in Human Centered Design and Engineering, and am currently seeking a Web Development position.",
+  address:"Seattle, Washington",
+  education:[
+    {
+      UniversityName:"University of Washington",
+      specialization:"Human Centered Design and Engineering",
+      MonthOfPassing:"June",
+      YearOfPassing:"2020",
+      Achievements:"Concentration in Human Computer Interaction, Minor in Architecture"
+    },
+    // {
+    //   UniversityName:"Some University",
+    //   specialization:"Some specialization",
+    //   MonthOfPassing:"Jan",
+    //   YearOfPassing:"2018",
+    //   Achievements:"Some Achievements"
+    // }
+  ],
+  work:[
+    {
+      CompanyName:"UW ",
+      specialization:"Instructor",
+      MonthOfLeaving:"Jan",
+      YearOfLeaving:"2018",
+      Achievements:"Some Achievements"
+    },
+    {
+      CompanyName:"Some Company",
+      specialization:"Some specialization",
+      MonthOfLeaving:"Jan",
+      YearOfLeaving:"2018",
+      Achievements:"Some Achievements"
+    }
+  ],
+  skillsDescription:"My skills:",
+  // skills:[
+  //   {
+  //     skillname:"Reactjs",
+  //     level:100,
+  //   },
+  //   {
+  //     skillname:"HTML5",
+  //     level:70,
+  //   },
+  //   {
+  //     skillname:"CSS",
+  //     level: 80,
+  //   },
+  // ],
+  portfolio:[
+    {
+      name:"project1",
+      description:"mobileapp",
+      imgurl:"images/portfolio/phone.jpg",
+      title:"Phone App",
+      githubLink:"https://github.com/Evanfeenstra/chatter",
+      fullDescription:"this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app "
+    },
+    {
+      name:"asdfasdfasf",
+      description:"mobileapp",
+      imgurl:"images/portfolio/weatherer.png",
+      title:"Something",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+    {
+      name:"project3",
+      description:"mobileapp",
+      imgurl:"images/portfolio/chatter.jpg",
+      title:"Hello",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+  ],
+}
+
+export default resumeData
